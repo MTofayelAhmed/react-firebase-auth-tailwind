4b64715fb1ac56a3003b907992962a4d6c7220d1
MTofayelAhmed <113205143+MTofayelAhmed@users.noreply.github.com>
2023-04-21 01:36:29 +0600

    login and register

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
-      <h2>this is  header</h2>
+      <div className="navbar bg-neutral text-neutral-content">
+        <a className="btn btn-ghost normal-case text-xl">Auth Master</a>
+        <Link className="btn btn-ghost normal-case text-xl" to="/">Home</Link>
+        <Link className="btn btn-ghost normal-case text-xl" to="/login">Login</Link>
+        <Link className="btn btn-ghost normal-case text-xl" to="/register">Register</Link>
+      </div>
     </div>
   );
 };
